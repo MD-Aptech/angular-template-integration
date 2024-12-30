f641a5162bae52634864cd900bb767c2f72a3b5f
MD-Aptech <155415513+MD-Aptech@users.noreply.github.com>
2024-12-30 20:29:18 +1000

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AboutcomponentComponent } from './aboutcomponent/aboutcomponent.component';
+import { EducationComponentComponent } from './education-component/education-component.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ClientComponent } from './client/client.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponentComponent,AboutcomponentComponent,EducationComponentComponent,SkillsComponent,ExperienceComponent,ProfilesComponent,PortfolioComponent,ClientComponent,ContactmeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/education-component/education-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-component',
+  imports: [],
+  templateUrl: './education-component.component.html',
+  styleUrl: './education-component.component.css'
+})
+export class EducationComponentComponent {
+
+}
